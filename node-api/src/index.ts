bc6223bee2ef272776a6374f2849126c019a2d7a
aronei44 <meliodasmeliodas224@gmail.com>
2024-04-30 15:17:33 +0700

    feat login and logout express

--- node-api/src/index.ts
+++ node-api/src/index.ts
@@ -12,6 +12,55 @@ app.get('/hello/:name', (req: Request, res: Response) => {
     res.send(`Hello, ${name}!`);
 });
 
+app.post('/v1/auth/login', (req: Request, res: Response) => {
+    const resp = {
+        "responseCode": "00",
+        "responseMessage": "Successfully",
+        "responseData": {
+            "pernr": "90144893",
+            "nip": "",
+            "sname": "Shavira Putri Virissya",
+            "werks": "KP00",
+            "btrtl": "KP01",
+            "kostl": "PS19019",
+            "orgeh": "70238340",
+            "stell": "",
+            "werksTx": "Kantor Pusat",
+            "btrtlTx": "Kantor Pusat",
+            "kostlTx": "DIVISI APPLICATION MANAGEMENT & OPERATIO",
+            "orgehTx": "APPLICATION MANAGEMENT & OPERATION DIVIS",
+            "stellTx": "Associate IT - Project Officer 1 Level 2",
+            "plansTx": "Associate IT - Project Officer 1 Level 2",
+            "jgpg": "",
+            "orgehPgs": "",
+            "plansPgs": "",
+            "orgehPgsTx": "",
+            "plansPgsTx": "",
+            "sisaCt": "",
+            "sisaCb": "",
+            "agama": "Islam",
+            "tipeUker": "KP",
+            "addArea": "KP",
+            "status": "",
+            "branch": "229",
+            "hilfm": "",
+            "htext": "",
+            "hilfmPgs": "",
+            "htextPgs": "",
+            "kawin": ""
+        }
+    };
+    res.send(resp);
+});
+
+app.post('/v1/auth/logout', (req: Request, res: Response) => {
+    const resp = {
+        "responseCode": "00",
+        "responseMessage": "Successfully"
+    };
+    res.send(resp);
+});
+
 app.listen(port, () => {
     console.log(`Server berjalan pada http://localhost:${port}`);
 });
